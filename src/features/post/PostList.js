1bdc76146bc750ab2534e53db58f39a00ed7c2f4
Nauval Yusuf Addairy <nauvalyusufad@gmail.com>
2022-06-23 09:40:27 +0700

    44:39

--- src/features/post/PostList.js
+++ src/features/post/PostList.js
@@ -4,16 +4,17 @@ import { selectAllPosts } from "./postSlice";
 import PostAuthor from "./PostAuthor";
 import TimeAgo from "./TimeAgo";
 import "./style.css";
-
+import ReactionButton from "./ReactionButton";
 const PostList = () => {
   const post = useSelector(selectAllPosts);
   const orderdPost = post.slice().sort((a, b) => b.date.localeCompare(a.date));
-  const renderedPost = post.map((post) => (
+  const renderedPost = orderdPost.map((post) => (
     <article key={post.id}>
       <h3>{post.title}</h3>
       <p>{post.content.substring(0, 100)}</p>
       <PostAuthor userId={post.userId} />
       <TimeAgo timeStamp={post.date} />
+      <ReactionButton post={post} />
     </article>
   ));
   return (
--- src/features/post/postSlice.js
+++ src/features/post/postSlice.js
@@ -6,12 +6,26 @@ const initialState = [
     title: "learnin redux toolkit",
     content: "i've heard a good things.",
     date: sub(new Date(), { minutes: 10 }).toISOString(),
+    reactions: {
+      thumbsUp: 0,
+      wow: 0,
+      heart: 0,
+      rocket: 0,
+      coffee: 0,
+    },
   },
   {
     id: "1",
     title: "slices......",
     content: "the more i say slice, the more i want pizza",
     date: sub(new Date(), { minutes: 5 }).toISOString(),
+    reactions: {
+      thumbsUp: 0,
+      wow: 0,
+      heart: 0,
+      rocket: 0,
+      coffee: 0,
+    },
   },
 ];
 const postSlice = createSlice({
@@ -30,12 +44,26 @@ const postSlice = createSlice({
             content,
             date: new Date().toISOString(),
             userId,
+            reaction: {
+              thumbsUp: 0,
+              wow: 0,
+              heart: 0,
+              rocket: 0,
+              coffee: 0,
+            },
           },
         };
+      },
+      reactionAdded(state, action) {
+        const { postId, reaction } = action.payload;
+        const existingPost = state.find((post) => post.id === postId);
+        if (existingPost) {
+          existingPost.reactions[reaction]++;
+        }
       },
     },
   },
 });
 export default postSlice.reducer;
 export const selectAllPosts = (state) => state.posts;
-export const { postAdded } = postSlice.actions;
+export const { postAdded, reactionAdded } = postSlice.actions;
